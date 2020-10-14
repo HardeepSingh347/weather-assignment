dd0f148149f8e41b5158f887ff9334d3ade789b4
HardeepSingh347 <dsekhon347@mail.com>
2020-10-14 12:37:42 +0530

    comments added

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,8 +5,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class WeatherService {
+  // http client injected in constructor
   constructor(private http: HttpClient) { }
 
+  // to get weather details according to city name
   getCityWeatherDetails = (cityName : string): any => {
     return this.http.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=db07828e9204966565c71d9b41fb786e`);
   }
--- src/app/weather-detail-list/weather-detail/weather-detail.component.ts
+++ src/app/weather-detail-list/weather-detail/weather-detail.component.ts
@@ -10,22 +10,30 @@ import { WeatherService } from 'src/app/services/weather.service';
 })
 export class WeatherDetailComponent implements OnInit, OnDestroy {
   weatherSubscription: Subscription;
+
+  // inpput to get city name from the parent component
   @Input() city: string;
+
   cityWeatherDetails: any;
   imgSrc: string;
 
+  // service dependency injected
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
+    // subscribe to service method to get city weather details
     this.weatherSubscription = this.weatherService.getCityWeatherDetails(this.city).subscribe(data => {
       if (data) {
         this.cityWeatherDetails = data;
+
+        // generating image src
         this.imgSrc = `http://openweathermap.org/img/wn/${this.cityWeatherDetails?.weather[0]?.icon}@4x.png`;
       }
     });
   }
 
   ngOnDestroy = () => {
+    // unsubscribe to avoid any memory leak issues
     if(this.weatherSubscription) {
       this.weatherSubscription.unsubscribe();
     }
